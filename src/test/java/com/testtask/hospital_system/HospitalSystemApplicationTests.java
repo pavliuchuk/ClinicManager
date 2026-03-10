de8f26fcf5c21d3b54696f1e669627955afefdd2
Ekaterina <ekaterina.pavliuchuk@tum.de>
2026-03-10 23:02:03 +0100

    initial project setup

---
+++ src/test/java/com/testtask/hospital_system/HospitalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testtask.hospital_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
